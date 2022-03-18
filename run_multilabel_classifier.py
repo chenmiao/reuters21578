0639bd3cf78e9b2d9d85e3235ecfe5dc4cb7f7f7
huangmimi <chenmiao@users.noreply.github.com>
2022-03-18 10:51:57 -0400

    commit on 3/18

--- run_multilabel_classifier.py
+++ run_multilabel_classifier.py
@@ -9,6 +9,7 @@ import csv
 import os
 import sys
 import tensorflow as tf
+import glob
 
 from sklearn.model_selection import train_test_split
 from collections import defaultdict
@@ -31,106 +32,106 @@ FLAGS = flags.FLAGS
 
 ## Required parameters
 flags.DEFINE_string(
-    "data_dir", None,
+    "reuters_data_dir", None,
     "The Reuters input data dir "
     "for the task.")
 
-flags.DEFINE_string(
-    "bert_config_file", None,
-    "The config json file corresponding to the pre-trained BERT model. "
-    "This specifies the model architecture.")
+# flags.DEFINE_string(
+#     "bert_config_file", None,
+#     "The config json file corresponding to the pre-trained BERT model. "
+#     "This specifies the model architecture.")
 
-flags.DEFINE_string("task_name", None, "The name of the task to train.")
+# flags.DEFINE_string("task_name", None, "The name of the task to train.")
 
-flags.DEFINE_string("vocab_file", None,
-                    "The vocabulary file that the BERT model was trained on.")
+# flags.DEFINE_string("vocab_file", None,
+#                     "The vocabulary file that the BERT model was trained on.")
 
-flags.DEFINE_string(
-    "output_dir", None,
-    "The output directory where the model checkpoints will be written.")
+# flags.DEFINE_string(
+#     "output_dir", None,
+#     "The output directory where the model checkpoints will be written.")
 
 ## Other parameters
 
-flags.DEFINE_string(
-    "init_checkpoint", None,
-    "Initial checkpoint (usually from a pre-trained BERT model).")
+# flags.DEFINE_string(
+#     "init_checkpoint", None,
+#     "Initial checkpoint (usually from a pre-trained BERT model).")
 
-flags.DEFINE_bool(
-    "do_lower_case", True,
-    "Whether to lower case the input text. Should be True for uncased "
-    "models and False for cased models.")
+# flags.DEFINE_bool(
+#     "do_lower_case", True,
+#     "Whether to lower case the input text. Should be True for uncased "
+#     "models and False for cased models.")
 
-flags.DEFINE_integer(
-    "max_seq_length", 128,
-    "The maximum total input sequence length after WordPiece tokenization. "
-    "Sequences longer than this will be truncated, and sequences shorter "
-    "than this will be padded.")
+# flags.DEFINE_integer(
+#     "max_seq_length", 128,
+#     "The maximum total input sequence length after WordPiece tokenization. "
+#     "Sequences longer than this will be truncated, and sequences shorter "
+#     "than this will be padded.")
 
-flags.DEFINE_bool("do_train", False, "Whether to run training.")
+# flags.DEFINE_bool("do_train", False, "Whether to run training.")
 
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+# flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
 
-flags.DEFINE_bool(
-    "do_predict", False,
-    "Whether to run the model in inference mode on the test set.")
+# flags.DEFINE_bool(
+#     "do_predict", False,
+#     "Whether to run the model in inference mode on the test set.")
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+# flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+# flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
-flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
+# flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
 
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+# flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
 
-flags.DEFINE_float("num_train_epochs", 3.0,
-                   "Total number of training epochs to perform.")
+# flags.DEFINE_float("num_train_epochs", 3.0,
+#                    "Total number of training epochs to perform.")
 
-flags.DEFINE_float(
-    "warmup_proportion", 0.1,
-    "Proportion of training to perform linear learning rate warmup for. "
-    "E.g., 0.1 = 10% of training.")
+# flags.DEFINE_float(
+#     "warmup_proportion", 0.1,
+#     "Proportion of training to perform linear learning rate warmup for. "
+#     "E.g., 0.1 = 10% of training.")
 
-flags.DEFINE_integer("save_checkpoints_steps", 1000,
-                     "How often to save the model checkpoint.")
+# flags.DEFINE_integer("save_checkpoints_steps", 1000,
+#                      "How often to save the model checkpoint.")
 
-flags.DEFINE_integer("iterations_per_loop", 1000,
-                     "How many steps to make in each estimator call.")
+# flags.DEFINE_integer("iterations_per_loop", 1000,
+#                      "How many steps to make in each estimator call.")
 
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
+# flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
 
-tf.flags.DEFINE_string(
-    "tpu_name", None,
-    "The Cloud TPU to use for training. This should be either the name "
-    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-    "url.")
+# tf.flags.DEFINE_string(
+#     "tpu_name", None,
+#     "The Cloud TPU to use for training. This should be either the name "
+#     "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
+#     "url.")
 
-tf.flags.DEFINE_string(
-    "tpu_zone", None,
-    "[Optional] GCE zone where the Cloud TPU is located in. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+# tf.flags.DEFINE_string(
+#     "tpu_zone", None,
+#     "[Optional] GCE zone where the Cloud TPU is located in. If not "
+#     "specified, we will attempt to automatically detect the GCE project from "
+#     "metadata.")
 
-tf.flags.DEFINE_string(
-    "gcp_project", None,
-    "[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+# tf.flags.DEFINE_string(
+#     "gcp_project", None,
+#     "[Optional] Project name for the Cloud TPU-enabled project. If not "
+#     "specified, we will attempt to automatically detect the GCE project from "
+#     "metadata.")
 
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+# tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
 
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+# flags.DEFINE_integer(
+#     "num_tpu_cores", 8,
+#     "Only used if `use_tpu` is True. Total number of TPU cores to use.")
 
-flags.DEFINE_integer(
-    "", 100,
-    "threshold of minimum number of samples for a reuters class"
-)
+# flags.DEFINE_integer(
+#     "", 100,
+#     "threshold of minimum number of samples for a reuters class"
+# )
 
-flags.DEFINE_string(
-    "task_name", "reuters",
-    "task name"
-)
+# flags.DEFINE_string(
+#     "task_name", "reuters",
+#     "task name"
+# )
 
 
 
@@ -814,7 +815,7 @@ def main(_):
 
 if __name__ == "__main__":
 
-    flags.mark_flag_as_required("data_dir")
+    flags.mark_flag_as_required("reuters_data_dir")
     flags.mark_flag_as_required("task_name")
     flags.mark_flag_as_required("vocab_file")
     flags.mark_flag_as_required("bert_config_file")
@@ -822,11 +823,11 @@ if __name__ == "__main__":
     tf.app.run()
 
     #python run_multilabel_classifier.py \
-    # --data_dir=""C:\Users\vivin\Documents\Projects\fidelity\reuters21578"" \
-    # --task_name="reuters" \
-    # --vocab_file="C:\Users\vivin\Documents\Projects\fidelity\models\uncased_L-12_H-768_A-12\uncased_L-12_H-768_A-12\vocab.txt" \
-    # --bert_config_file="C:\Users\vivin\Documents\Projects\fidelity\models\uncased_L-12_H-768_A-12\uncased_L-12_H-768_A-12\bert_config.json" \
-    # --output_dir="C:\Users\vivin\Documents\Projects\fidelity\models\model_20220317"
+    # --data_dir=C:\Users\vivin\Documents\Projects\fidelity\reuters21578 \
+    # --task_name=reuters \
+    # --vocab_file=C:\Users\vivin\Documents\Projects\fidelity\models\uncased_L-12_H-768_A-12\uncased_L-12_H-768_A-12\vocab.txt \
+    # --bert_config_file=C:\Users\vivin\Documents\Projects\fidelity\models\uncased_L-12_H-768_A-12\uncased_L-12_H-768_A-12\bert_config.json \
+    # --output_dir=C:\Users\vivin\Documents\Projects\fidelity\models\model_20220317
 
 
 
